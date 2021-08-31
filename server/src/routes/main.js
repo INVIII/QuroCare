0d69b9f7c827bef5d5af1cb813c0b57080667b9e
Lawful2002 <sharshvardhan101@gmail.com>
2021-08-31 18:42:51 +0530

    Updated Project Structure & cleaned up code

--- server/src/routes/main.js
+++ server/src/routes/main.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('home')
+  res.render('./pages/home')
 })
 
 module.exports = router
