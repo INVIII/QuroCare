da7d32296fa314374b9ae1f4c6a34c328796c8e6
Lawful2002 <sharshvardhan101@gmail.com>
2021-08-31 16:00:21 +0530

    added routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const mainRoutes = require('./routes/main.js')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '..', '..', 'web', 'views'))
 
 app.use(express.static(path.join(__dirname, '..', '..', 'web', 'public')))
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
+app.use('/', mainRoutes)
 
 app.listen(3000, () => {
   console.log('Server running')
