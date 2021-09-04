e91b103c6a92eb88dfc9f15e9a89d0e10153ab67
Lawful2002 <sharshvardhan101@gmail.com>
2021-09-04 14:02:03 +0530

    Updated files

--- server/src/routes/main.js
+++ server/src/routes/main.js
@@ -2,6 +2,14 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
+  res.render('./pages/home')
+})
+
+router.get('/login', (req, res) => {
+  res.render('./pages/login-test')
+})
+
+router.get('/register', (req, res) => {
   res.render('./pages/register')
 })
 
