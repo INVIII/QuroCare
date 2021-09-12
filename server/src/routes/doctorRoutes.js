481915de5d515855fe44bbcd442b09dc64208844
Ayushsunny <ayush100anand@gmail.com>
2021-09-13 01:14:38 +0530

    Minor changes

--- server/src/routes/doctorRoutes.js
+++ server/src/routes/doctorRoutes.js
@@ -7,6 +7,28 @@ router.get('/login', (req, res) => {
   res.render('./pages/login')
 })
 
+router.post('/login', (req, res) => {
+  const { email, pass } = req.body
+  let password = ''
+  const q = `SELECT * FROM doctor WHERE email = "${email}" `
+  connection.query(q, (err, result) => {
+    if (err) throw err
+    const doctor=result[0]
+    if (result.length === 0) {
+      console.log('User Not Found')
+      res.redirect('/doctors/login')
+    } else {
+      password = result[0].password
+      if (password === pass) {
+        res.render('./pages/doctorDashboard',{doctor})
+      } else {
+        console.log('Wrong Password')
+        res.redirect('/doctors/login')
+      }
+    }
+  })
+})
+
 router.get('/dashboard', (req, res) => {
   res.render('./pages/doctorDashboard')
 })
