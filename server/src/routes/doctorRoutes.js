48167ba9bfb8f5c09a50979d0c72409ea307d063
Lawful2002 <sharshvardhan101@gmail.com>
2021-09-12 16:01:12 +0530

    added basic login logic

--- server/src/routes/doctorRoutes.js
+++ server/src/routes/doctorRoutes.js
@@ -3,8 +3,12 @@ const router = express.Router()
 
 const connection = require('../sqlConnector')
 
+router.get('/login', (req, res) => {
+  res.render('./pages/login')
+})
+
 router.get('/dashboard', (req, res) => {
-  res.render('./pages/doctordash')
+  res.render('./pages/doctorDashboard')
 })
 
 module.exports = router
--- server/src/routes/patientRoute.js
+++ server/src/routes/patientRoute.js
@@ -3,4 +3,33 @@ const router = express.Router()
 
 const connection = require('../sqlConnector')
 
+router.get('/login', (req, res) => {
+  res.render('./pages/login')
+})
+
+router.post('/login', (req, res) => {
+  const { email, pass } = req.body
+  let password = ''
+  const q = `SELECT password FROM patient WHERE email = "${email}" `
+  connection.query(q, (err, result) => {
+    if (err) throw err
+
+    if (result.length === 0) {
+      console.log('User Not Found')
+      res.redirect('/patient/login')
+    } else {
+      password = result[0].password
+      if (password === pass) {
+        res.render('./pages/patientdash')
+      } else {
+        res.redirect('/patient/login')
+      }
+    }
+  })
+})
+
+router.get('/register', (req, res) => {
+  res.render('./pages/register')
+})
+
 module.exports = router
