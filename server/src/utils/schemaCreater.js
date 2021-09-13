f303eff0360b52347cb5b342e75a2fea280a34d3
Dev5902 <devsharma5902@gmail.com>
2021-09-13 13:39:09 +0530

    Schema refined

--- server/src/utils/schemaCreater.js
+++ server/src/utils/schemaCreater.js
@@ -2,7 +2,7 @@ const connection = require('./sqlConnector')
 
 const queries = []
 
-queries.push('CREATE TABLE patient (fname varchar(15) NOT NULL, lname varchar(15) NOT NULL, email varchar(50), phone varchar(13), gender varchar(15), password varchar(100), PRIMARY KEY(email));', 'CREATE TABLE doctor (fname varchar(15) NOT NULL, lname varchar(15) NOT NULL, department varchar(15) NOT NULL, phone varchar(13) NOT NULL, password varchar(100) NOT NULL, email varchar(50), PRIMARY KEY(email));')
+queries.push('CREATE TABLE patient(_id varchar(25) NOT NULL, fname varchar(15) NOT NULL, lname varchar(15) NOT NULL, email varchar(50), phone varchar(13), gender varchar(15), password varchar(255), PRIMARY KEY(email));', 'CREATE TABLE doctor ( _id varchar(25) NOT NULL ,fname varchar(15) NOT NULL, lname varchar(15) NOT NULL, department varchar(15) NOT NULL, phone varchar(13) NOT NULL, password varchar(255) NOT NULL, email varchar(50), fees INT(10) ,PRIMARY KEY(email));')
 
 for (const i of queries) {
   connection.query(i, function (err, res) {
--- server/src/utils/seeder/seeder.js
+++ server/src/utils/seeder/seeder.js
@@ -1,5 +1,6 @@
 const faker = require('faker')
 const connection = require('../sqlConnector')
+const {nanoid} = require('nanoid')
 
 function random (a, b) {
   return Math.floor(Math.random() * a) + b
@@ -11,19 +12,19 @@ const patData = []
 const docData = []
 
 for (let i = 0; i < 10; i++) {
-  patData.push([faker.name.firstName(), faker.name.lastName(), faker.internet.email(), faker.phone.phoneNumber(), gender[random(3, 0)], faker.internet.password()])
+  patData.push([nanoid() ,faker.name.firstName(), faker.name.lastName(), faker.internet.email(), faker.phone.phoneNumber(), gender[random(3, 0)], faker.internet.password()])
 }
 
 for (let i = 0; i < 10; i++) {
-  docData.push([faker.name.firstName(), faker.name.lastName(), deps[random(6, 0)], faker.phone.phoneNumber(), faker.internet.password(), faker.internet.email(), random(500, 300)])
+  docData.push([nanoid() ,faker.name.firstName(), faker.name.lastName(), deps[random(6, 0)], faker.phone.phoneNumber(), faker.internet.password(), faker.internet.email(), random(500, 300)])
 }
 
-connection.query('INSERT INTO patient (fname, lname, email, phone, gender, password) VALUES ?', [patData], function (err, res) {
+connection.query('INSERT INTO patient (_id, fname, lname, email, phone, gender, password) VALUES ?', [patData], function (err, res) {
   if (err) throw err
   console.log(res)
 })
 
-connection.query('INSERT INTO doctor (fname, lname, department, phone, password, email, fees) VALUES ?', [docData], function (err, res) {
+connection.query('INSERT INTO doctor (_id, fname, lname, department, phone, password, email, fees) VALUES ?', [docData], function (err, res) {
   if (err) throw err
   console.log(res)
 })
