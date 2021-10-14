13006f912e7aba461d769adc04071157dfe1325d
Harshvardhan Singh <73544247+Lawful2002@users.noreply.github.com>
2021-10-14 20:41:13 +0530

    Update sqlConnector.js

--- server/src/utils/sqlConnector.js
+++ server/src/utils/sqlConnector.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-  host: 'remotemysql.com',
-  user: '787yC9r1m0',
-  password: 'uLk5oGqfuY',
-  database: '787yC9r1m0'
+  host: '',
+  user: '',
+  password: '',
+  database: ''
 })
 
 module.exports = connection
