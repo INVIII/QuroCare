d0bf7303e2d240c6710bd951984d44b36dfafcb6
Lawful2002 <sharshvardhan101@gmail.com>
2021-10-14 20:08:41 +0530

    Configured Online DB

--- server/src/utils/sqlConnector.js
+++ server/src/utils/sqlConnector.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'hms'
+  host: 'remotemysql.com',
+  user: '787yC9r1m0',
+  password: 'uLk5oGqfuY',
+  database: '787yC9r1m0'
 })
 
 module.exports = connection
