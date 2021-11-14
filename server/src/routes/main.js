ae58ea70c33e3b036cef78a786b268ba678b90be
Harshvardhan Singh <73544247+Lawful2002@users.noreply.github.com>
2021-11-14 06:17:45 -0800

    Book test (#62)
    * added the room addition part without protcted login
    
    * Book test (#60)
    
    * passing data successfully to the allocate room page
    
    * the form part of allocateroom.ejs is breaking and is required to be fixed
    
    * minor fix
    
    * successfully added the room allocation part
    
    * Book test (#61)
    
    * passing data successfully to the allocate room page
    
    * the form part of allocateroom.ejs is breaking and is required to be fixed
    
    * minor fix
    
    * successfully added the room allocation part
    
    * added the prescription page
    
    Co-authored-by: Dev5902 <devsharma5902@gmail.com>
    Co-authored-by: Dev Sharma <89684672+Dev5902@users.noreply.github.com>
    Co-authored-by: Lawful2002 <sharrshvardhan101@gmail.com>

--- server/src/routes/main.js
+++ server/src/routes/main.js
@@ -1,5 +1,7 @@
+const e = require('express')
 const express = require('express')
 const router = express.Router()
+const connection = require('../utils/sqlConnector')
 
 router.get('/', (req, res) => {
   res.render('./pages/home', { success: req.flash('success'), error: req.flash('error') })
@@ -7,6 +9,112 @@ router.get('/', (req, res) => {
 
 router.get('/admin', (req, res) => {
   res.render('./pages/admin')
+})
+
+router.get('/allocateroom', (req, res) => {
+  // eslint-disable-next-line no-unused-vars
+  let rooms = []
+  let q = 'SELECT roomid FROM room WHERE room.occupied = 0'
+  connection.query(q, (err1, result1) => {
+    if (err1) {
+      console.log(err1)
+      req.flash('error', 'An error has occured please contact Admin')
+      res.redirect('/')
+    } else {
+      rooms = result1
+      q = 'SELECT patient.fname AS p_fname, patient.lname AS p_lname, patient._id AS pat_id, doctor.fname AS d_fname, doctor.lname AS d_lname, waiting._id as waitingId FROM patient, doctor, waiting WHERE waiting.pat_id = patient._id AND waiting.doc_id = doctor._id'
+      connection.query(q, (err, result2) => {
+        if (err) {
+          console.log(err)
+          req.flash('error', 'An error has occured please contact Admin')
+          res.redirect('/')
+        } else {
+          const pat = result2
+          if (rooms.length === 0) {
+            req.flash('warning', 'There are currently no free rooms')
+          }
+          res.render('./pages/allocateroom', {
+            pat,
+            rooms,
+            warning: req.flash('warning')
+          })
+        }
+      })
+    }
+  })
+})
+
+router.get('/addroom', (req, res) => {
+  res.render('./pages/addroom', { warning: req.flash('warning'), success: req.flash('success') })
+})
+
+router.post('/addroom', (req, res) => {
+  const { roomID } = req.body
+  let q = `SELECT * FROM room WHERE roomid = '${roomID}'`
+  connection.query(q, (err, result) => {
+    if (err) {
+      console.log(err)
+      req.flash('error', 'An error has occured please contact Admin')
+      res.redirect('/')
+    }
+    if (result.length > 0) {
+      req.flash('warning', 'Please enter an unique id for the Room')
+      res.redirect('/addroom')
+    } else {
+      q = `INSERT INTO room (roomid, occupied) VALUES ('${roomID}',0)`
+      connection.query(q, (err1, result1) => {
+        if (err1) {
+          console.log(err)
+          req.flash('error', 'An error has occured please contact Admin')
+          res.redirect('/')
+        } else {
+          req.flash('success', 'Room added successfully')
+          res.redirect('/addroom')
+        }
+      })
+    }
+  })
+})
+
+router.post('/allocateroom', (req, res) => {
+  const { patient, room } = req.body
+  let q = `INSERT INTO occupies (pat_id, roomid) VALUES ('${patient}','${room}')`
+  connection.query(q, (err1, result1) => {
+    if (err1) {
+      console.log(err1)
+      req.flash('error', 'An error has occured please contact Admin')
+      res.redirect('/')
+    } else {
+      q = `DELETE FROM waiting WHERE pat_id = '${patient}'`
+      connection.query(q, (err1, result2) => {
+        if (err1) {
+          console.log(err1)
+          req.flash('error', 'An error has occured please contact Admin')
+          res.redirect('/')
+        } else {
+          q = `UPDATE patient SET admitted = 1 WHERE _id = '${patient}'`
+          connection.query(q, (err1, result3) => {
+            if (err1) {
+              console.log(err1)
+              req.flash('error', 'An error has occured please contact Admin')
+              res.redirect('/')
+            } else {
+              q = `UPDATE room SET occupied = 1 WHERE roomid = '${room}'`
+              connection.query(q, (err1, result4) => {
+                if (err1) {
+                  console.log(err1)
+                  req.flash('error', 'An error has occured please contact Admin')
+                  res.redirect('/')
+                } else {
+                  res.redirect('/allocateroom')
+                }
+              })
+            }
+          })
+        }
+      })
+    }
+  })
 })
 
 module.exports = router
--- server/src/utils/config.js
+++ server/src/utils/config.js
@@ -1,6 +1,2 @@
 module.exports = {
-    host: 'HOST',
-    user: 'USER',
-    password: 'PASSWORD',
-    database: 'DATABASE'
-};
+}
